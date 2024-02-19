f8b54e6082e6173486fc43d019a22c5b682b1b88
Nicolas Burbano <nicolas.rnbp@gmail.com>
2024-02-19 10:17:01 -0500

    responsive menu

--- src/Componentes/FoodItem.jsx
+++ src/Componentes/FoodItem.jsx
@@ -4,38 +4,36 @@ import React from 'react';
 
 const FoodItem = ({ producto }) => {
   return (
-    <div key={producto.id} style={{ display: 'flex', width: '40%', margin: '10px', textAlign: 'left', 
-      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', borderRadius: '5px', overflow: 'hidden'}}>
-      <img src={producto.imagenProducto} alt={`Imagen: ${producto.nombre}`} style={{ width: '200px', height: '200px', 
-        objectFit: 'cover' }} tabIndex={0}/>
-      <div style={{ paddingLeft: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', width: '60%' }}>
-        <h3 style={{ fontSize: '28px',marginTop: '-4px'}} tabIndex={0} aria-label={`Nombre menu: 
+    <div key={producto.id} className='col-sm-12 col-lg-6 ' >
+      <div className='d-flex flex-column flex-lg-row product d-flex align-items-center' style={{
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', borderRadius: '5px', overflow: 'hidden'
+      }}>
+        <img src={producto.imagenProducto} alt={`Imagen: ${producto.nombre}`} className='product-imagen' tabIndex={0} />
+        <div className='p-3 w-100'>
+          <h3 className='h3' tabIndex={0} aria-label={`Nombre menu: 
         ${producto.nombre}`}> {producto.nombre} </h3>
-        <p style={{ 
-          fontSize: '20px',
-          marginTop: '-25px'}} tabIndex={0} aria-label={`Descripcion del menu: 
+          <p tabIndex={0} aria-label={`Descripcion del menu: 
           ${producto.descripcion}`}>
-              {producto.descripcion}</p>
-        <p style={{ 
-          fontSize: '24px',
-          marginTop: '-20px',
-          textAlign: 'center'}} tabIndex={0} aria-label={`Precio del menu: 
-          ${producto.precio} dolares`}> US${producto.precio}</p>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '-30px'}}>
-          <button 
-            style={{ 
-              padding: '10px', 
-              backgroundColor: '#1D3E43',
-              width: '60%',
-              textAlign: 'center',
-              color: '#E8D2A3', 
-              border: 'none', 
-              borderRadius: '5px', 
-              cursor: 'pointer',
-              fontSize: '16px'
-            }}
-          > Añadir al carrito <img src="../../../Imagenes/iconoBotonCarro.png" alt="" style={{height: '25px', marginBottom: '-8px', paddingLeft: '18px'}}/>
-          </button>
+            {producto.descripcion}</p>
+          <p style={{
+            textAlign: 'center'
+          }} tabIndex={0} aria-label={`Precio del menu: 
+        ${producto.precio} dolares`}> <strong>US${producto.precio}</strong> </p>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <button
+              style={{
+                padding: '10px',
+                backgroundColor: '#1D3E43',
+                width: '70%',
+                textAlign: 'center',
+                color: '#E8D2A3',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+              }}
+            > Añadir al carrito <img src="../../../Imagenes/iconoBotonCarro.png" alt="" style={{ height: '25px', paddingLeft: '18px' }} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/Componentes/SeccionComida.jsx
+++ src/Componentes/SeccionComida.jsx
@@ -5,18 +5,20 @@ export const SeccionComida = () => {
   return (
     <>
       {data.secciones.map((seccion) => (
-        <seccion id={seccion.id} key={seccion.id} aria-label={`Seccion ${seccion.nombre}`} tabIndex={0}>
-          {/* <img src={seccion.imagenSeccion} alt={seccion.nombre} width="100%" height="302px" tabIndex={0}/> */}
+        <seccion id={seccion.id} key={seccion.id} aria-label={`Seccion ${seccion.nombre}`} tabIndex={0} className='hv-100 '>
+          {<img src={seccion.imagenSeccion} alt={seccion.nombre} width="100%" tabIndex={0} style={{ objectFit: "cover", minHeight: 300, display:'block'}} />}
           <h2 style={{ fontSize: '26px', color: '#1D3E43', paddingLeft: '40px' }} tabIndex={1}>{seccion.nombre}</h2>
-          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', paddingBottom: '70px' }}>
-            {seccion.productos.map((producto) => (
-              <FoodItem key={producto.id} producto={producto} />
-            ))}
+          <div className="container overflow-hidden my-5">
+            <div className='row g-5'>
+              {seccion.productos.map((producto) => (
+                <FoodItem key={producto.id} producto={producto} />
+              ))}
+            </div>
           </div>
           <hr style={{
-            border: '1px solid #ddd', /* Puedes ajustar el estilo de la línea aquí */
+            border: '5px solid #000',
             width: '100%',
-            margin: '10px 0', /* Puedes ajustar el margen superior e inferior */
+            margin: 0
           }} />
         </seccion>
       ))}
